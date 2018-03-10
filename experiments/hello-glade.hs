9fcdaecdf1cc1219adbfa6fb82502b847c5b9dc3
Ben Powell <benjamindavidpowell@gmail.com>
2018-03-10 15:21:12 -0500

    Glade demo now works

--- experiments/hello-glade.hs
+++ experiments/hello-glade.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified GI.Gtk as Gtk
+import Data.GI.Base.ManagedPtr (unsafeCastTo)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
   Gtk.init Nothing
 
   builder <- Gtk.builderNewFromFile "app-interface-alpha.glade"
-  win <- Gtk.builderGetObject builder "mainWindow"
+  win <- Gtk.builderGetObject builder "mainWindow" >>= unsafeCastTo Gtk.Window . fromJust
+  Gtk.onWidgetDestroy win Gtk.mainQuit  
+  
+  Gtk.widgetShowAll win
+  Gtk.main
 
   --win <- Gtk.new Gtk.Window [ Gtk.windowTitle Gtk.:= "Glade demo" ]
-  --Gtk.onWidgetDestroy win Gtk.mainQuit  
-
-  --Gtk.widgetShowAll win
-  Gtk.main
